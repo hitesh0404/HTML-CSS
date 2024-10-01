cf0eb0a14b626b47c1cb705b85eb6021d0827ebe
hitesh <hiteshkukaramprajapati@gmail.com>
2024-10-01 09:52:13 +0530

    Todo list Completed

--- Batch 414 JS/todo.js
+++ Batch 414 JS/todo.js
@@ -1,6 +1,7 @@
 document.getElementById('add_task').addEventListener('click',
-    function(event){
-        task = document.getElementById('task').value
+    function(){
+        t = document.getElementById('task')
+        task =t.value
         // console.log(task)
         const todo_list = document.getElementById('todo_list')
         check = document.createElement('input')
@@ -9,12 +10,20 @@ document.getElementById('add_task').addEventListener('click',
         check.classList.add('task_list')
         li = document.createElement('li')
         task_text  =  document.createTextNode(task)
+        delete_button = document.createElement('button')
+        // delete_button.value = 'DELETE'
+        // console.log(delete_button);
+        delete_text = document.createTextNode('DELETE')
+        delete_button.appendChild(delete_text)
+        delete_button.style.marginLeft ='40px'
+        delete_button.classList.add('delete_button')
         li.appendChild(check)
         li.appendChild(task_text)
+        li.appendChild(delete_button)
         todo_list.appendChild(li)
+        t.value = ''
     }
 )
-
 document.getElementById('todo_list').addEventListener('click',
     function(event){
         event.target.classList.toggle('done')
@@ -28,11 +37,8 @@ document.getElementById('todo_list').addEventListener('click',
             else
                 t.firstChild.checked=true;        
         }
-
-
     }
 )
-
 function check_do(event){
     if (t.checked)
         event.target.parentElement.classList.add('done')
@@ -44,3 +50,16 @@ document.getElementById('todo_list').addEventListener('click',
     function(event){
         t  = event.target
         event.target.addEventListener('click',check_do(event) )  })
+
+
+
+document.getElementById('todo_list').addEventListener('click',
+    function(event){
+        t  = event.target
+        if (t.tagName.toLowerCase() === "button" ){
+            if (t.classList.contains('delete_button')){
+                t.parentElement.remove()
+            }
+        }
+    }
+)
